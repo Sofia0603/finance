4b21237d74abdbc29addb160f0b63d690ee552b9
Sofia Mac <shestakova.sofia2016@mail.ru>
2025-11-27 23:54:41 +0400

    added ne

---
+++ frontend/src/scripts/dashboard.js
@@ -0,0 +1,50 @@
+// $(function() {
+//
+//   rome(input_from, {
+//     dateValidator: rome.val.beforeEq(input_to),
+//     time: false
+//   });
+//
+//   rome(input_to, {
+//     dateValidator: rome.val.afterEq(input_from),
+//     time: false
+//   });
+//
+//
+// });
+
+var pieChartCanvas = $('#pieChart').get(0).getContext('2d')
+var pieChartCanvas2 = $('#pieChart2').get(0).getContext('2d')
+
+var pieData= {
+  labels: [
+    'Red',
+    'Orange',
+    'Yellow',
+    'Green',
+    'Blue',
+  ],
+  datasets: [
+    {
+      data: [700,500,400,600,300,100],
+      backgroundColor : ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc', '#d2d6de'],
+    }
+  ]
+};
+
+var pieOptions     = {
+  maintainAspectRatio : false,
+  responsive : true,
+}
+//Create pie or douhnut chart
+// You can switch between pie and douhnut using the method below.
+new Chart(pieChartCanvas, {
+  type: 'pie',
+  data: pieData,
+  options: pieOptions
+})
+new Chart(pieChartCanvas2, {
+  type: 'pie',
+  data: pieData,
+  options: pieOptions
+})
